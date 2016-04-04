f7ee22ef169ce1df1e4bbdbb3088052fa3f3cea8
Hojo Yu <yu.hojo.lqq3@gmail.com>
2016-04-04 23:18:35 +0900

    [modify] adding clone dein

--- ubuntu/bootstrap.sh
+++ ubuntu/bootstrap.sh
@@ -2,8 +2,11 @@
 
 mkdir $HOME/.config
 mkdir -p $HOME/.config/nvim
+mkdir -p $HOME/.config/nvim/dein
 
 echo 'export XDG_CONFIG_HOME=$HOME/.config' >> $HOME/.bashrc
 
 cp ../nvimrc $HOME/.config/nvim/init.vim
 cp ../.bashrc $HOME/.bashrc
+dein_dir = $HOME/.config/nvim/dein/repos/github.com/Shougo/dein.vim
+git clone https://github.com/Shougo/dein.vim $dein_dir
